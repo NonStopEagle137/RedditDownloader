fd217b89c0c3b6c60938f71cbe086c23c70798ac
AthrvaPandhare108137 <48444746+NonStopEagle137@users.noreply.github.com>
2021-06-03 17:46:05 +0530

    Create master_script.py

---
+++ master_script.py
@@ -0,0 +1,18 @@
+# Support Script for reddit Scraping.
+import pandas as pd
+import json
+import subprocess
+import os
+import sys
+import time
+def get_assets(number = 100):
+    sub_reddits = ['r/meme'] # list of all good meme-subreddit accounts goes here.
+    path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    for sub_reddit in sub_reddits:
+        bat_code = r"""python redditdownloader {} --limit={}""".format(sub_reddit, str(number));       
+        jump_directory = r"""cd {}""".format(path) 
+        #bat_code = bat_code.encode('utf-8')
+        print('cmd /c {} && {}'.format(jump_directory,bat_code))
+        os.system('cmd /c {} && {}'.format(jump_directory,bat_code))
+        
+get_assets(50)
