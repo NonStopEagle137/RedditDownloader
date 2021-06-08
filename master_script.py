51df66af1904aaeb347e22671828d512f99d7cdf
AthrvaPandhare108137 <48444746+NonStopEagle137@users.noreply.github.com>
2021-06-08 20:16:29 +0530

    Update master_script.py

--- master_script.py
+++ master_script.py
@@ -10,8 +10,10 @@ def get_assets(number = 100):
     sub_reddits = list(sub_reddits.split(','))
     #sub_reddits = ['r/meme'] # list of all good meme-subreddit accounts goes here.
     path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    filters = input("Enter any one filter : Options are (top,hot,new,controversial,gilded,rising)")
+    filters = str(filters)
     for sub_reddit in sub_reddits:
-        bat_code = r"""python redditdownloader {} --type 'top' --limit={}""".format(sub_reddit, str(number));       
+        bat_code = r"""python redditdownloader {} --type {} --limit={}""".format(sub_reddit, filters, str(number));       
         jump_directory = r"""cd {}""".format(path) 
         #bat_code = bat_code.encode('utf-8')
         print(' {} ; {}'.format(jump_directory,bat_code))
