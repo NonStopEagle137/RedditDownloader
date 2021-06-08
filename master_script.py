90f08ee9638cf962a44fc9ffc6cdfb77df1f7c1d
AthrvaPandhare108137 <48444746+NonStopEagle137@users.noreply.github.com>
2021-06-08 19:51:45 +0530

    Update master_script.py

--- master_script.py
+++ master_script.py
@@ -11,7 +11,7 @@ def get_assets(number = 100):
     #sub_reddits = ['r/meme'] # list of all good meme-subreddit accounts goes here.
     path = os.path.abspath(os.path.dirname(sys.argv[0]))
     for sub_reddit in sub_reddits:
-        bat_code = r"""python redditdownloader {} --limit={}""".format(sub_reddit, str(number));       
+        bat_code = r"""python redditdownloader {} --type 'top' --limit={}""".format(sub_reddit, str(number));       
         jump_directory = r"""cd {}""".format(path) 
         #bat_code = bat_code.encode('utf-8')
         print(' {} ; {}'.format(jump_directory,bat_code))
